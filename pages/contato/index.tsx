285b9528c2b341207a4e50b1deac7a5d9fc5e411
DanielSantos <daniel.correa321@hotmail.com>
2023-03-24 14:34:06 -0300

    commit4

--- pages/contato/index.tsx
+++ pages/contato/index.tsx
@@ -7,7 +7,7 @@ const Contato = () => {
                 <h2>Contate-me</h2>
                 <input type="text" placeholder='Nome' />
                 <input type="text" placeholder='Assunto' />
-                <textarea name="" id="" cols="30" rows="10" placeholder='Comente aqui'></textarea>
+                <textarea name="" id="" placeholder='Comente aqui'></textarea>
             </div>
         </div>
     )
